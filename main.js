b5b89ea112227206b0584e7a3c0c05e1a9f0d4ec
xyyojl <m13631897256@163.com>
2018-12-09 12:55:46 +0800

    用简单的代码理解无缝轮播实现的思路

--- main.js
+++ main.js
@@ -1,73 +1,62 @@
 /*
     思路：
-    1. 无缝轮播有3种状态。显示在前面的界面，离开，进入
-    2. 要初始化，把第一张图片显示给用户看，其他图片保持enter的状态
-    3. 第一张图片离开，第二张图片显示给用户看，第三张图片还是保持enter状态
-    4. 第二张图片离开，第三张图片显示给用户看，第一张图片还是保持enter状态
-    5. 第三张图片离开，第一张图片显示给用户看，第二张图片还是保持enter状态
-    6. 需要getImage，x，初始化，makeCurrent，makeLeave，makeEnter
 
-    应用到的知识
-    
+*/
+let $buttons = $('#buttonWrapper > button');
+let $slides = $('#slides');
+let current = 0;    //当前的索引
+let $images = $slides.children('img');
 
- */
+makeFakeSlides()
+$slides.css({transform:'translateX(-400px)'})
+bindEvents()
 
-let n
-init()
-var timer = setInterval(() => {
-    makeLeave(getImage(n))
-        .one('transitionend',(e) => {
-            makeEnter($(e.currentTarget))
-        })
-    makeCurrent(getImage(n+1))
-    n += 1
-},3000)
-document.addEventListener('visibilitychange',function(e){
-    if(document.hidden){
-        window.clearInterval(timer)
-    }else{
-        timer = setInterval(() => {
-            makeLeave(getImage(n))
-                .one('transitionend',(e) => {
-                    makeEnter($(e.currentTarget))
+
+function bindEvents(){
+    $buttons.eq(0).on('click',function(){
+        if(current === 2){
+            console.log('说明你是从最后一张到第一张')
+            //你既然是做了一个假的1，执行完之后，马上用真的代替
+            $slides.css({transform:'translateX(-1600px)'})
+                .one('transitionend',function(){
+                    console.log(1)
+                    $slides.hide()
+                        .offset()
+                    $slides.css({transform:'translateX(-400px)'}).show()
+                    
                 })
-            makeCurrent(getImage(n+1))
-            n += 1
-        },3000) 
-    }
-})
-
-
-
-function manageData(n){
-    if(n>5){
-        n = n % 5;
-        if(n === 0){
-            n = 5
+        }else{
+            $slides.css({transform:'translateX(-400px)'})
         }
-    } // n = 1 2 3 4 5
-    return n
+        current = 0
+    })
+    $buttons.eq(1).on('click',function(){
+        console.log(current)
+        $slides.css({transform:'translateX(-800px'})
+        current = 1
+    })
+    $buttons.eq(2).on('click',function(){
+        if(current === 0){
+            console.log('说明你是从第一张到最后一张')
+            $slides.css({transform:'translateX(-0px)'})
+                .one('transitionend',function(){
+                    console.log(1)
+                    $slides.hide()
+                        .offset()
+                    $slides.css({transform:'translateX(-1200px)'}).show()
+                    
+                })
+        }else{
+            $slides.css({transform:'translateX(-1200px)'})
+        }
+        current = 2
+    })
 }
 
-function init(){
-    n = 1
-    $('.images > img:nth-child(1)').addClass('current')
-    .siblings().addClass('enter')
-}
 
-
-function getImage(n){
-    return $(`.images > img:nth-child(${manageData(n)})`);
+function makeFakeSlides(){
+    let $firstCopy = $images.eq(0).clone(true)
+    let $lastCopy = $images.eq($images.length-1).clone(true)
+    $slides.append($firstCopy)
+    $slides.prepend($lastCopy)
 }
-
-function makeCurrent($node){
-    return $node.removeClass('leave enter').addClass('current')
-}
-function makeLeave($node){
-    return $node.removeClass('current enter').addClass('leave')
-}
-function makeEnter($node){
-    return $node.removeClass('current leave').addClass('enter')
-}
-
-
